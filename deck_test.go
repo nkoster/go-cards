b8cb0776e32fea42a54435088ad5d20135932ca8
Niels Koster <n.koster@portavita.eu>
2020-11-07 18:59:52 +0100

    Extend TestNewDeck

--- deck_test.go
+++ deck_test.go
@@ -2,10 +2,23 @@ package main
 
 import "testing"
 
+const CARDSNEEDED = 52
+const FIRSTCARD = "Ace of Hearts"
+const LASTCARD = "King of Clubs"
+
 func TestNewDeck(t *testing.T) {
-	const CardsNeeded = 52
+
 	d := newDeck()
-	if len(d) != CardsNeeded {
-		t.Errorf("Deck does not hold %d cards but %v", CardsNeeded, len(d))
+	if len(d) != CARDSNEEDED {
+		t.Errorf("\nDeck does not hold %d cards but %v", CARDSNEEDED, len(d))
 	}
+
+	if d[0] != FIRSTCARD {
+		t.Errorf("\nDeck does not have %s as the first card, but %v", FIRSTCARD, d[0])
+	}
+
+	if d[len(d)-1] != LASTCARD {
+		t.Errorf("\nDeck does not have %s as the last card, but %v", LASTCARD, d[len(d)-1])
+	}
+
 }
